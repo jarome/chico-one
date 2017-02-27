bd558fa1d4895ce2efcd3806b477a1d79f270a9a
Jarome Harmsworth <jarome@Jaromes-MacBook-Pro-3.local>
2017-02-27 14:59:23 +1100

    Updates to html

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var minifyCSS = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('autoprefixer');
 var precss = require('precss');
 var postcssSimpleVars = require('postcss-simple-vars');
@@ -9,12 +9,15 @@ var postcssNesting = require('postcss-nesting');
 
 gulp.task('css', function () {
   var processors = [
-    autoprefixer({browsers: ['last 4 version']}),
+    autoprefixer,
     postcssSimpleVars,
     precss,
     postcssNesting
   ];
   return gulp.src('./src/css/style.css')
     .pipe(postcss(processors))
+    .pipe(cleanCSS())
     .pipe(gulp.dest('./dist/css'));
 });
+
+gulp.watch('./src/css/*.css', ['css']);
